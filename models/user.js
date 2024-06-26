12accde52a27a117ba501338d09558df365d4624
Eric Ibu <ericomartin.0785@gmail.com>
2024-06-26 15:48:02 +0100

    refactor: create branch

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profession: { type: String, required: false },
+    gender: { type: String, required: false },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true }, timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
