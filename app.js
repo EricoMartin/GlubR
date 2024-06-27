1901d3cc92871fa6d1c853ec5a76d8fdf4b968ea
Eric Ibu <ericomartin.0785@gmail.com>
2024-06-27 11:06:10 +0100

    feat(auth): user can login

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
-const CONFIG = require("./config/config");
-const dbConnection = require("./db/connection");
-const authRoute = require("./routes/auth");
+import CONFIG from "./config/config";
+import dbConnection from "./db/connection";
+import authRoute from "./routes/auth";
 
 const port = parseInt(CONFIG.PORT, 10) || 3000;
 
--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs/dist/bcrypt";
 import User from "../models/user";
+import jwt from "jsonwebtoken";
 
 class AuthController {
   static async createUser(req, res, next) {
@@ -66,13 +67,52 @@ class AuthController {
       let newUserObj = newUser.toObject();
       const { id, ...rest } = newUserObj;
       //Return response
-      return res.send({
+      return res.status(201).json({
         status: "success",
         user: rest,
       });
     } catch (e) {
       console.log(e.message);
     }
+  }
+
+  static async login(req, res) {
+    const { email, password } = req.body;
+    if (!email) {
+      res.status(400).json({ error: "Missing email" });
+      return;
+    }
+    if (!password) {
+      res.status(400).json({ error: "Missing password" });
+      return;
+    }
+    const oldUser = await User.findOne({ email });
+    if (!oldUser) {
+      res.status(404).json({ error: "Unknown user. Please signup" });
+      return;
+    }
+    const checkPassword = await bcrypt.compare(password, oldUser.password);
+    if (!checkPassword) {
+      res.status(404).json({ error: "Incorrect details" });
+      return;
+    }
+    const token = jwt.sign(
+      { user_id: oldUser._id, email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      }
+    );
+    const { ...all } = oldUser.toObject();
+    const data = {
+      token,
+      all,
+    };
+
+    return res.status(200).header("authorization", token).json({
+      status: 200,
+      data,
+    });
   }
 }
 
